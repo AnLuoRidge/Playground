a502478649ccf148f730167ddffb1c84f800708d
Steven Ding <anluoridge@gmail.com>
2018-09-16 10:21:42 +1000

    huge refactor

---
+++ TestEvaluation/TestEvaluation/ExpressionParser.cs
@@ -0,0 +1,376 @@
+﻿using System;
+//using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace TestEvaluation
+{
+    public class ExpressionParser
+    {
+        public static List<Element> ParseCharacters(string exp)
+        {
+            var parsedExpression = new List<Element>();
+            // parse single char
+            foreach (var ch in exp.ToCharArray())
+            {
+                // Num
+                int num;
+                if (int.TryParse(ch.ToString(), out num))
+                {
+                    parsedExpression.Add(new Element(ElementType.Num, num, ch.ToString()));
+                }
+           
+                // Unown
+                if (char.IsLetter(ch))
+                {
+                    parsedExpression.Add(new Element(ElementType.Unknown, 1, ch.ToString()));
+                }
+                // =
+                if (ch == '=')
+                {
+                    parsedExpression.Add(new Element(ElementType.Equals, null, ch.ToString()));
+                }
+                // +
+                if (ch == '+')
+                {
+                    parsedExpression.Add(new Element(ElementType.Plus, null, ch.ToString()));
+                }
+                // -
+                if (ch == '-' || ch == '–')
+                {
+                    parsedExpression.Add(new Element(ElementType.Minus, null, ch.ToString()));
+                }
+                // *
+                if (ch == '*')
+                {
+                    parsedExpression.Add(new Element(ElementType.Multiple, null, ch.ToString()));
+                }
+                // /
+                if (ch == '/')
+                {
+                    parsedExpression.Add(new Element(ElementType.Division, null, ch.ToString()));
+                }
+                // (
+                if (ch == '(')
+                {
+                    parsedExpression.Add(new Element(ElementType.LeftBracket, null, ch.ToString()));
+                }
+                // )
+                if (ch == ')')
+                {
+                    parsedExpression.Add(new Element(ElementType.RightBracket, null, ch.ToString()));
+                }
+            }
+
+            return parsedExpression;
+        }
+
+        // deal with a*X, such as 2X 
+        public static List<Element> CombineCoefficientToUnknown(List<Element> exp)
+        {
+            var parsedExpression = exp;
+            for (int i = parsedExpression.Count - 1; i >= 0; i--)
+            {
+                var element = parsedExpression[i];
+                if (i > 0 && (element.Type == ElementType.Unknown))
+                {
+                    var lastElement = parsedExpression[i - 1];
+                    if (lastElement.Type == ElementType.Num)
+                    {
+                        parsedExpression[i] = new Element(element.Type, lastElement.Value, lastElement.Print + element.Print);
+                        parsedExpression.RemoveAt(i - 1);
+                    }
+                }
+            }
+
+            return parsedExpression;
+        }
+        public static List<Element> CombineDigits(List<Element> exp)
+        {
+            var combinedExp = new List<Element>();
+            var combinedDigits = string.Empty;
+
+            for (var i = 0; i < exp.Count; i++)
+            {
+                if (exp[i].Type == ElementType.Num)
+                {
+                    combinedDigits += exp[i].Print;
+                    if (i == exp.Count - 1)
+                    {
+                        combinedExp.Add(new Element(ElementType.Num, int.Parse(combinedDigits), combinedDigits));
+                    }
+                }
+                else
+                {
+                    if (combinedDigits.Length > 0)
+                    {
+                        combinedExp.Add(new Element(ElementType.Num, int.Parse(combinedDigits), combinedDigits));
+                    }
+                    combinedExp.Add(exp[i]);
+                    combinedDigits = "";
+                }
+            }
+            return combinedExp;
+        }
+
+        public static List<Element> RevertMinus(List<Element> exp)
+        {
+            var parsedExpression = exp;
+
+            for (int i = parsedExpression.Count - 1; i >=0 ; i--)
+            {
+                var element = parsedExpression[i];
+                if (element.Type == ElementType.Minus) //  exception: && i < parsedExpression.Count - 1
+                {
+                    var nextNum = parsedExpression[i + 1];
+                    if (nextNum.Type == ElementType.Num ||
+                        nextNum.Type == ElementType.Unknown)
+                    {
+                        parsedExpression[i + 1] = new Element(nextNum.Type, -nextNum.Value, "-" + nextNum.Print);
+                        // deal with minus symbol
+                        if (i > 0)
+                        {
+                            // if * - OR / -, remove '-'
+                            var lastElement = parsedExpression[i - 1];
+                            if (lastElement.Type == ElementType.Multiple ||
+                                lastElement.Type == ElementType.Division)
+                            {
+                                parsedExpression.RemoveAt(i);
+                            }
+                            // replace '-' with '+'
+                            else
+                            {
+                                parsedExpression[i] = new Element(ElementType.Plus, null, "+");
+                            }
+                            
+                        }
+
+                    }
+                }
+            }
+            return parsedExpression;
+        }
+
+        public static List<Element> RemoveBrackets(List<Element> exp)
+        {
+            var parsedExpression = exp;
+            
+            // record index of brackets
+            var bracketLocations = new List<int>();
+            for (var i = 0; i < parsedExpression.Count; i++)
+            {
+                if (parsedExpression[i].Type == ElementType.LeftBracket ||
+                    parsedExpression[i].Type == ElementType.RightBracket)
+                {
+                    bracketLocations.Add(i);
+                }
+            }
+            
+        for (var i = bracketLocations.Count - 2; i >= 0; i = i - 2)
+        {
+            var leftBracketIndex = bracketLocations[i];
+            var rightBracketIndex = bracketLocations[i + 1];
+
+            // Handle 2(, but index 0 is not (
+            if (leftBracketIndex > 0)
+            {
+                var coeffcientBeforeBracket = parsedExpression[leftBracketIndex - 1];
+                // if there is a coefficient before (
+                if (coeffcientBeforeBracket.Type == ElementType.Num)
+                {
+                    // update the factors by multipled by the outside coefficient
+                    for (var j = leftBracketIndex + 1; j < rightBracketIndex; j=j+2)
+                    {
+                        var updatedValue = coeffcientBeforeBracket.Value * parsedExpression[j].Value;
+
+                        // TODO: DELETE - update printValue
+                        string updatedPrintValue;
+                        if (parsedExpression[j].Type == ElementType.Num)
+                        {
+                            updatedPrintValue = updatedValue.ToString();
+                        } else if (parsedExpression[j].Type == ElementType.Unknown)
+                        {
+                            updatedPrintValue = updatedValue + "X";
+                        } else
+                        {
+                            updatedPrintValue = parsedExpression[j].Print;
+                        }
+
+                        // TODO: X (), ()/X
+                        ElementType updatedType = parsedExpression[j].Type;
+                         // if X (...) OR if 0 (...)
+//                        switch ((int) lastElement.Type * (int) parsedExpression[j].Type)
+//                        {
+//                            case 1:
+//                                updatedType = ElementType.Num;
+//                                break;
+//                            case 2:
+//                                updatedType = ElementType.Unknown;
+//                                break;
+//                            default:
+//                                updatedType =  parsedExpression[j].Type;
+//                                break;
+//                        }
+//                        
+                        parsedExpression[j] = new Element(updatedType, updatedValue, updatedPrintValue);
+                    }
+                    
+                    // if (...) / X OR X / X
+
+                    // 不查后面还有没有括号，直接去括号。
+//                    parsedExpression[leftBracketIndex] = new Element(ElementType.RightBracket, null, string.Empty);
+//                    parsedExpression[rightBracketIndex] = new Element(ElementType.LeftBracket, null, string.Empty);
+                    // TODO remove?
+                    parsedExpression.RemoveAt(rightBracketIndex);
+                    parsedExpression.RemoveAt(leftBracketIndex);
+                    
+                    // remove the coefficient before the bracket
+                        parsedExpression[leftBracketIndex-1] = new Element(ElementType.Num, 0, ""); // diff? string.empty
+//                        parsedExpression.RemoveAt(leftBracketIndex-1);
+                }
+
+                // )(
+            }
+
+            if (rightBracketIndex < parsedExpression.Count - 2)
+            {
+                if (exp[rightBracketIndex+1].Type == ElementType.Division)
+                {
+                    var coefficentAfterBracket = exp[rightBracketIndex + 2].Value;
+                    // update by divided by the coefficient
+                    for (var j = leftBracketIndex + 1; j < rightBracketIndex; j = j + 2)
+                    {
+                        var updatedValue = 1 / coefficentAfterBracket.Value * parsedExpression[j].Value;
+
+                        // TODO: DELETE - update printValue
+                        string updatedPrintValue;
+                        if (parsedExpression[j].Type == ElementType.Num)
+                        {
+                            updatedPrintValue = updatedValue.ToString();
+                        } else if (parsedExpression[j].Type == ElementType.Unknown)
+                        {
+                            updatedPrintValue = updatedValue + "X";
+                        } else
+                        {
+                            updatedPrintValue = parsedExpression[j].Print;
+                        }
+
+                        // TODO: X (), ()/X
+                        ElementType updatedType = parsedExpression[j].Type;
+                        // if X (...) OR if 0 (...)
+//                        switch ((int) lastElement.Type * (int) parsedExpression[j].Type)
+//                        {
+//                            case 1:
+//                                updatedType = ElementType.Num;
+//                                break;
+//                            case 2:
+//                                updatedType = ElementType.Unknown;
+//                                break;
+//                            default:
+//                                updatedType =  parsedExpression[j].Type;
+//                                break;
+//                        }
+//                        
+                        parsedExpression[j] = new Element(updatedType, updatedValue, updatedPrintValue);
+                    }
+                                        
+                    // if (...) / X OR X / X
+
+                    parsedExpression.RemoveAt(rightBracketIndex+2);
+                    parsedExpression.RemoveAt(rightBracketIndex+1);
+                    // TODO remove?
+                    parsedExpression.RemoveAt(rightBracketIndex);
+                    parsedExpression.RemoveAt(leftBracketIndex);
+                }
+            }
+        }
+
+            return parsedExpression;
+        }
+        public static List<Element> Divide(List<Element> exp)
+        {
+            var parsedExpression = exp;
+            for (int i = 0; i < parsedExpression.Count; i++)
+            {
+                var element = parsedExpression[i];
+                if (element.Type == ElementType.Division && i < parsedExpression.Count - 1)
+                {
+                    var nextElement = parsedExpression[i + 1];
+                    // TODO: enum byte
+                    if (nextElement.Type == ElementType.Num ||
+                        nextElement.Type == ElementType.Unknown)
+                    {
+                        // TODO: update print
+                        if (nextElement.Type == ElementType.Unknown)
+                        {
+                            parsedExpression[i + 1] = new Element(ElementType.ReciprocalUnknown, 1 / nextElement.Value, "1/" + nextElement.Print);
+                        }
+                        else
+                        {
+                            parsedExpression[i + 1] = new Element(nextElement.Type, 1 / nextElement.Value, "1/" + nextElement.Print);
+                        }
+                        parsedExpression[i] = new Element(ElementType.Multiple, null, "*");
+                    }
+                }
+            }
+
+            return parsedExpression;
+        }
+
+        public static List<Element> Multiple(List<Element> exp)
+        {
+            var parsedExpression = exp;
+            for (int i = parsedExpression.Count - 1; i >= 0; i--)
+            {
+                var element = parsedExpression[i];
+                if (element.Type == ElementType.Multiple)
+                {
+                    var nextElement = parsedExpression[i + 1];
+                    var lastElement = parsedExpression[i - 1];
+                    var product = lastElement.Value * nextElement.Value;
+                    ElementType updatedType;
+                    
+                    switch ((int) lastElement.Type * (int) nextElement.Type)
+                    {
+                        case 1:
+                        case 6:
+                            updatedType = ElementType.Num;
+                            parsedExpression[i - 1] = new Element(updatedType, product, product.ToString());
+                            break;
+                        case 2:
+                            updatedType = ElementType.Unknown;
+                            parsedExpression[i - 1] = new Element(updatedType, product, product+"X");
+                            break;
+                        case 3:
+                            updatedType = ElementType.ReciprocalUnknown;
+                            parsedExpression[i - 1] = new Element(updatedType, product, product+"1/X");
+                            break;
+                        default:
+                            updatedType = nextElement.Type;
+                            parsedExpression[i - 1] = new Element(updatedType, product, product.ToString());
+                            break;
+                    }
+                    parsedExpression.RemoveAt(i + 1);
+                    parsedExpression.RemoveAt(i);
+                }
+            }
+
+            return parsedExpression;
+        }
+        
+        public static List<Element> RemoveRedundantOperators(List<Element> exp)
+        {
+            // if the same operator appear more than twice
+            return exp;
+        }
+
+        public static void PrintExpressionWhen(string leading, List<Element> exp)
+        {
+            Console.WriteLine(leading);
+            foreach (var item in exp)
+            {
+                Console.Write(item.Print + " ");
+            }
+        }
+    }
+}
--- TestEvaluation/TestEvaluation/Tests.cs
+++ TestEvaluation/TestEvaluation/Tests.cs
@@ -1,270 +1,174 @@
 ﻿using System;
 using NUnit.Framework;
+using System.Collections.Generic;
 
 namespace TestEvaluation
 {
     [TestFixture]
     public class Tests
     {
-        private EquationSolver _equationSolver = new EquationSolver();
-        public QuadraticEquation _quadraticEquation = new QuadraticEquation();
+//        private EquationSolver _equationSolver = new EquationSolver();
 
         [Test]
-        public void Test1()
+        public void CombineDigits1()
         {
-            UnknownShouldBe((1, null), "X + 2 = 3");
+            var exp = ExpressionParser.CombineDigits(new List<Element>
+            {
+                new Element(ElementType.Num, 7, "7"), 
+                new Element(ElementType.Num, 2, "2"),
+                new Element(ElementType.Multiple, 0, "x"),
+                new Element(ElementType.RightBracket, 0, "("),
+                new Element(ElementType.RightBracket, 0, "5"),
+            });
+            var expection = new List<Element>()
+            {
+                new Element(ElementType.Num, 72, "72"), 
+                new Element(ElementType.Multiple, 0, "x"),
+                new Element(ElementType.RightBracket, 0, "("),
+                new Element(ElementType.RightBracket, 0, "5"),
+            };
+            Assert.AreEqual(exp, expection);
         }
         
         [Test]
-        public void Test2()
+        public void CombineDigitsShouldUnchangeSingleDigits()
         {
-            UnknownShouldBe((1, -11), "(X + 5)(X + 5) = 36");
+            var exp = ExpressionParser.CombineDigits(new List<Element>
+            {
+                new Element(ElementType.Unknown, 1, "X"), 
+                new Element(ElementType.Plus, 0, "+"),
+                new Element(ElementType.Num, 2, "2"),
+                new Element(ElementType.Equals, 0, "="),
+                new Element(ElementType.Num, 6, "6"),
+            });
+            var expection = new List<Element>()
+            {
+                new Element(ElementType.Unknown, 1, "X"), 
+                new Element(ElementType.Plus, 0, "+"),
+                new Element(ElementType.Num, 2, "2"),
+                new Element(ElementType.Equals, 0, "="),
+                new Element(ElementType.Num, 6, "6"),
+            };
+            Assert.AreEqual(exp, expection);
         }
-        
-        [Test]
-        public void Test3()
+                [Test]
+        public void A1()
         {
-            UnknownShouldBe((1, -4), "(X + 1)(X + 2) = 6");
+            UnknownShouldBe(4, "X+2=6");
         }
-        
-        [Test]
-        public void Test4()
+                [Test]
+        public void A2()
         {
-            UnknownShouldBe((3, null), "X - 1 = 2");
+            UnknownShouldBe(5, "3X-6=9");
         }
-        
-        [Test]
-        public void Test5()
+                [Test]
+        public void A3()
         {
-            UnknownShouldBe((2, null), "X * 2 = 4");
+            UnknownShouldBe(3.75f, "X = 5*X – 5 * 3");
         }
-        
-        [Test]
-        public void Test6()
+                [Test]
+        public void A4()
         {
-            UnknownShouldBe((8, null), "X / 2 = 4");
+            UnknownShouldBe(1, "5(2) + 5x = 15");
         }
-        
-        [Test]
-        public void Ass1()
+                [Test]
+        public void A5()
         {
-            UnknownShouldBe((4, null), "X + 2 = 6");
+            UnknownShouldBe(30, "X/5=6");
         }
-        
-        [Test]
-        public void Ass2()
+                [Test]
+        public void A6()
         {
-            UnknownShouldBe((5, null), "3X - 6 = 9");
+            UnknownShouldBe(2, "(X-2) = 0");
         }
-        
-        [Test]
-        public void Ass3()
+
+                [Test]
+        public void A7()
         {
-            UnknownShouldBe(((float)3.75, null), "X = 5*X – 5 * 3");
+            UnknownShouldBe(4, "4(4X) + 2(X) = 72");
         }
-        
-        [Test]
-        public void Ass4()
+
+                        [Test]
+        public void A8()
         {
-            UnknownShouldBe((1, null), "5(2) + 5x = 15");
+            UnknownShouldBe(14, "2X + 8 = 4X - 20");
         }
-        
-        [Test]
-        public void Ass5()
+
+                                [Test]
+        public void A9()
         {
-            UnknownShouldBe((30, null), "X/5=6");
+            UnknownShouldBe(2, "X = 14 - - - 8X + 4");
         }
-        
-        [Test]
-        public void Ass6()
+                                [Test]
+        public void A10()
         {
-            UnknownShouldBe((-3, -4), "x^2 + 7 * x + 6 * 2 = 0");
+            UnknownShouldBe(5, "4 = 2 ( X – 3 )");
         }
-        
-        [Test]
-        public void Ass7()
+                                [Test]
+        public void A11()
         {
-            UnknownShouldBe((3, 2), "(X-2) (X-3) = 0");
+            UnknownShouldBe(-1, "X = 2 + 3X");
         }
-        
-        [Test]
-        public void Ass8()
+                                [Test]
+        public void A12()
         {
-            UnknownShouldBe((4, null), "4(4X) + 2 (X) = 72");
+            UnknownShouldBe(6, "2X = 4X - 12");
         }
-        
-        [Test]
-        public void Ass9()
+                                [Test]
+        public void A13()
         {
-            UnknownShouldBe((13, null), "2(X-1)+8=4*X-20");
+            UnknownShouldBe(null, "24 = 6X / 3X");
         }
-        
-        [Test]
-        public void Ass10()
+                                [Test]
+        public void A14()
         {
-            UnknownShouldBe((3, -3), "4x^2 – 11 * 2 = x^2 + 5");
-        }
-        
-        [Test]
-        public void Ass11()
-        {
-            UnknownShouldBe((71, null), "X = 5 + 22 * 3");
-        }
-        
-        [Test]
-        public void Ass12()
-        {
-            UnknownShouldBe((71, null), "X = 5 + 22 * 3");
-        }
-        
-        [Test]
-        public void Ass13()
-        {
-            UnknownShouldBe(((float)5/3, null), "3X = 5");
-        }
-        
-        [Test]
-        public void Ass14()
-        {
-            UnknownShouldBe(((float)5/3, null), "5 = 3X");
-        }
-        
-        [Test]
-        public void Ass15()
-        {
-            UnknownShouldBe((8, null), "5 + 3 = X");
-        }
-        
-        [Test]
-        public void Ass16()
-        {
-            UnknownShouldBe((-3, null), "X = -6 + 3");
+            UnknownShouldBe(4, "X = (5 + 3) / 2");
         }
 
         [Test]
-        public void Ass17()
+             public void A15()
         {
-            UnknownShouldBe((-3, null), "X = 3 + -6");
+            UnknownShouldBe(-1/3f, "5 = 3(X+2)");
         }
-        
         [Test]
-        public void Ass18()
+                public void A16()
         {
-            UnknownShouldBe((32, null), "24 = 6X / 8");
+            UnknownShouldBe(32, "24 = 6X / 8");
         }
-        
         [Test]
-        public void Ass19()
+                public void A17()
         {
-            UnknownShouldBe((-1, null), "6 / 3 = 5 + 3X");
+            UnknownShouldBe(-1, "6 / 3 = 5 + 3X");
+        }
+        [Test]
+                public void A18()
+        {
+            UnknownShouldBe(11, "6 + 10 / 2 = X");
+        }
+        [Test]
+                public void A19()
+        {
+            UnknownShouldBe(71, "X = 5 + 22 * 3");
+        }
+        [Test]
+                public void A20()
+        {
+            UnknownShouldBe(-3, "X = -6 + 3");
+        }
+        [Test]
+                public void A21()
+        {
+            UnknownShouldBe(5f/3, "5 = 3X");
+        }
+        [Test]
+                public void A22()
+        {
+            UnknownShouldBe(-3, "X = 3 + -6");
         }
 
-        [Test]
-        public void Ass20()
+        private void UnknownShouldBe(float? expected, string expression)
         {
-            UnknownShouldBe((11, null), "6 + 10 / 2 = X");
-        }
-        
-        [Test]
-        public void Ass21()
-        {
-            UnknownShouldBe((-1, null), "X = 2 + 3X");
-        }
-
-        [Test]
-        public void Ass22()
-        {
-            UnknownShouldBe((6, null), "2X = 4X - 12");
-        }
-        
-        [Test]
-        public void Ass23()
-        {
-            UnknownShouldBe((null, null), "24 = 6X / 3X");
-        }
-        
-        [Test]
-        public void Ass24()
-        {
-            UnknownShouldBe((4, null), "X = (5 + 3) / 2");
-        }
-        
-        [Test]
-        public void Ass25()
-        {
-            UnknownShouldBe(((float)-1.75, null), "5 = 3(X+2)");
-        }
-        
-        [Test]
-        public void Ass26()
-        {
-            UnknownShouldBe((13, null), "6 = X + -7");
-        }
-                             
-        [Test]
-        public void Ass27()
-        {
-            UnknownShouldBe((-40, null), "X = 5 * -8");
-        }
-        [Test]
-        public void Ass28()
-        {
-            UnknownShouldBe((11, null), "4 = 2 ( X – 3 )");
-        }
-        [Test]
-        public void Ass29()
-        {
-            UnknownShouldBe((5, null), "X = 3 + 12 / 6 +");
-        }
-        [Test]
-        public void Ass30()
-        {
-            UnknownShouldBe((2, null), "X = 14 - - - 8X + 4");
-        }
-        [Test]
-        public void Self1()
-        {
-            UnknownShouldBe((2, null), "X = 14 - - - 8X + 4");
-        }
-//        
-//        [Test]
-//        public void Ass31()
-//        {
-//            UnknownShouldBe((11, null), "6 + 10 / 2 = X");
-//        }
-//        [Test]
-//        public void Ass32()
-//        {
-//            UnknownShouldBe((11, null), "6 + 10 / 2 = X");
-//        }
-//        [Test]
-//        public void Ass33()
-//        {
-//            UnknownShouldBe((11, null), "6 + 10 / 2 = X");
-//        }
-//        [Test]
-//        public void Ass34()
-//        {
-//            UnknownShouldBe((11, null), "6 + 10 / 2 = X");
-//        }
-//        [Test]
-//        public void Ass35()
-//        {
-//            UnknownShouldBe((11, null), "6 + 10 / 2 = X");
-//        }
-        
-        
-        
-
-//        UnknownShouldBe((3, null), "X-1=2");// X 后面什么都不匹配
-
-//        UnknownShouldBe((1, null), "2(X + 2) = 6");
-
-        //            UnknownShouldBe(1, "(X + 2)(X - 3) + 4 / 2 = 3*X^2");
-
-        private void UnknownShouldBe((float?, float?) expected, string expression)
-        {
-            Assert.AreEqual(expected, _equationSolver.Calc(expression));
+            Assert.AreEqual(expected, EquationSolver.Calc(expression));
         }
     }
 }
