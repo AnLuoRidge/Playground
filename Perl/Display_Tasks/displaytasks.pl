28fd25f5200ad546b1d82ad61772e146c0521d4c
Steven Ding <anluoridge@gmail.com>
2019-05-24 12:34:45 +1000

    Use Getopt::Long

--- Perl/Display_Tasks/displaytasks.pl
+++ Perl/Display_Tasks/displaytasks.pl
@@ -1,10 +1,50 @@
 #!/usr/bin/perl
+# use strict;
+use Getopt::Long;
+# use Switch;
 
-open INFILE, "<$ARGV[1]" or die "Failed to open file.";
+# command line flags/options
+# my %options=();
+# GetOptions("a:m:t:s:n", \%options);
 
+GetOptions ("all=s" => \$file,
+			"file=s"   => \$data,      # string
+			"verbose"  => \$verbose,   # flag
+			"sreshhold=s{2}" => \@args)
+or die("Error in command line arguments\n");
+
+sort_tasks($file) if $file;
+print "-s @args\n" if @args;
+
+# sort_tasks($options{a}) if defined $options{a};
+# print "-m $options{m}\n" if defined $options{m};
+# print "-t $options{t}\n" if defined $options{t};
+# print "-s $options{s}\n" if defined $options{s};
+# print "-n $options{n}\n" if defined $options{n};
+
+# other things found on the command line
+# print "Other things found on the command line:\n" if $ARGV[0];
+# foreach (@ARGV)
+# {
+#   print "$_\n";
+# }
+
+# switch ($ARGV[0]) {
+# 	case "-a" { sort_tasks() }
+# 	case "-m" { total_memory_size() }
+# 	case "-t" { total_cpu_time() }
+# 	case "-s" { memory_threshold($ARGV[1]) }
+# 	case "-n" { student_info() }
+# 	else { printf "Unknown option.\n"}
+# }
+
+sub read_file {
+open INFILE, "<$_[0]" or die "Failed to open file.";
+
+$isNull = 0;
 # check if empty
-if (-z "$ARGV[1]") {
-die "No tasks found\n" ;
+if (-z "$_[0]") {
+$isNull = 1;
 }
 
 # parse
@@ -22,17 +62,55 @@ while (<INFILE>) {
 	push(@tasks, \%info);
 	#print $tasks[0]->{program_name};
 }
-
+}
 
 # printf "\n\n unsorted:";
 # printf "$tasks[0]->{program_name} \n";
 # printf "$tasks[1]->{program_name} \n";
 
 # printf "\nsorted";
-@sorted_tasks = sort { lc($a->{program_name}) cmp lc($b->{program_name}) } @tasks;
+
+
+sub sort_tasks {
+	read_file($_[0]);
+	if ($isNull) {
+		printf "No tasks found\n";
+		exit;
+	}
+	@sorted_tasks = sort { lc($a->{program_name}) cmp lc($b->{program_name}) } @tasks;
 
 # print $tasks[2]->{program_name};
 
-foreach (@sorted_tasks) {
-	printf "$_->{process_id} $_->{memory_size} $_->{cpu_time} $_->{program_name}\n";
+	foreach (@sorted_tasks) {
+		printf "$_->{process_id} $_->{memory_size} $_->{cpu_time} $_->{program_name}\n";
+	}
+}
+
+sub total_memory_size {
+	if ($isNull) {
+		printf "No memory used\n";
+		exit;
+	}
+	print "Total memory size: ???\n";
+}
+
+sub total_cpu_time {
+	if ($isNull) {
+		printf "No CPU time used\n";
+		exit;
+	}
+	printf "Total CPU time: seconds\n";
+}
+
+sub memory_threshold {
+	if ($isNull) {
+		printf "No tasks with the specified memory size\n";
+		exit;
+	}
+
+	printf "$_[0]";
+}
+
+sub student_info {
+	printf "Name: Xinyuan Ding\nStudent ID: 12873687\nCompletion Date: 26 May 2019\n";
 }
--- Perl/Display_Tasks/test.pl
+++ Perl/Display_Tasks/test.pl
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-printf "=== FAIL TO OPEN FILE (EMPTY) ===\n"
-assert=$(./displaytasks.pl -a ./taskfile_empty.txt 2>&1)
+set count = 1;
+printf "=== NO TASKS FOUND ===\n"
+assert=`./displaytasks.pl -a ./taskfile_empty.txt`
 echo $assert
 if [ "$assert" = "No tasks found" ] ; then
 	printf "[PASS]\n"
@@ -12,7 +13,7 @@ fi
 printf "\n=== UNREADABLE ===\n"
 assert=$(./displaytasks.pl -a ./taskfile_unreadable.txt 2>&1)
 echo $assert
-if [ "$assert" = "Failed to open file. at ./displaytasks.pl line 3." ] ; then
+if [ "$assert" = "Failed to open file. at ./displaytasks.pl line 5." ] ; then
 	printf "[PASS]\n"
 else
 	printf "[FAIL]\n"
@@ -20,6 +21,7 @@ fi
 
 printf "\n=== PRINT ALL ===\n"
 assert=`./displaytasks.pl -a ./taskfile_normal.txt`
+echo "$assert"
 # echo -n "$assert" | hd
 correct=`printf "4 0 6 events\n22355 1970 2009 find\n1 4730 1031782 init\n21413 5928 1 sshd\n2190 450 0 top"`
 # echo -n "$correct" | hd
