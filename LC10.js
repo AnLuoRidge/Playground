c3c4da38c2b82f8a5c5a72b8cf2f14db5714b674
Alan Ridge <anluoridge@gmail.com>
2019-11-12 16:23:48 +1100

    Use patterns to indicate fail or pass

--- LC10.js
+++ LC10.js
@@ -24,6 +24,7 @@ var isMatch = (s, p) => {
     // create a empty or with sth length-defined array
     // var foo = new Array(45); // create an empty array with length 45
     
+    // const v = 0; // verbose
 
     var resultFlags = new Array(s.split('').length);
     resultFlags.forEach(flag => {
@@ -75,7 +76,12 @@ const test = (cases) => {
     const testCase = cases[key];
     console.log(`${testCase.s} | ${testCase.p}`);
     if (isMatch(testCase.s, testCase.p) === testCase.a) {
+      console.log('[PASS]');
+      console.log('---------------------------------------------')
       passCount ++;
+    } else {
+      console.log('[FAIL]');
+      console.log('---------------------')
     }
   }
   const caseLength = Object.keys(cases).length;
@@ -86,7 +92,8 @@ const test = (cases) => {
 const cases = {
   fullMatch: {s: 'aa', p: 'aa', a: true}, 
   fullMatch2: {s: 'aa', p: 'ab', a: false},
-  dot: {s: 'aa', p: 'a.', a: true}
+  dot: {s: 'aa', p: 'a.', a: true},
+  asterisk: {s: 'me', p: 'me*', a: true}
 };
 
 // run test
