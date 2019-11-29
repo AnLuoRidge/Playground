79df4b9f7b4ab1e9d7a1ad398297b084e4a85467
anluoridge <anluoridge@gmail.com>
2019-11-29 13:55:32 +1100

    2nd try of LC-26

--- leetcode/26-remove-duplicates–from–sorted–array.ts
+++ leetcode/26-remove-duplicates–from–sorted–array.ts
@@ -3,8 +3,24 @@
  * @return {number}
  */
 
+ // 2nd try✅️ (64 ms, 36.7 MB): Comparing array[i-1] with array[i]
+ // Removed extra console.log() to save time.
+ const removeDuplicates = (input) => {
+    let uniqueCount = 1;
+    for (let i = 1; i < input.length; i++) {
+      if (input[i] !== input[i - 1]) {
+        input[uniqueCount] = input[i];
+        uniqueCount++;
+      }
+    }
+    console.log(`${uniqueCount}: [${input}]`);
+    return uniqueCount;
+  }
+
  // 1st try✅️ (404 ms, 42.9 MB): Using ES7 `Array.includes()`
  // Without the notice of SORTED array
+
+ /*
  const removeDuplicates = (input: number[]) => {
      let uniqueCount: number = 1; 
      for (let i = 1; i < input.length; i ++) {
@@ -17,8 +33,9 @@
     console.log(`${uniqueCount}: [${input}]`);
     return uniqueCount;
   }
-  
-  // let input1 = [1, 1, 2];
-  // let input2 = [1, 1, 1, 2, 2, 2, 2, 3];
+  */
+
+  // const input1 = [1, 1, 2];
+  // const input2 = [1, 1, 1, 2, 2, 2, 2, 3];
   // removeDuplicates(input1);
   // removeDuplicates(input2);
